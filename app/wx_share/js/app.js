65d38b0e61d7c8ff01090d174965704f5485c089
mengtailu <tailu1993@163.com>
2016-06-07 13:22:28 +0800

    活动确认投保页+领取成功页+活动投保成功页

--- app/wx_share/js/app.js
+++ app/wx_share/js/app.js
@@ -26,6 +26,9 @@ function($routeProvider) {
   }).when('/freebd', {
     templateUrl: 'wx_share/partials/bd_part.html',
     controller: 'wxShareBdCtrl'
+  }).when('/recognizee', {
+    templateUrl: 'wx_share/partials/recognizee.html',
+    controller: 'wxSharerecognizeeCtrl'
   }).when('/couponlist', {
     templateUrl: 'wx_share/partials/coupon_list.html',
     controller: 'myCouponListCtrl'
