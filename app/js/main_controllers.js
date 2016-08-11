64188ce18320d346a3df13ee902553321e460aff
fanzhang <fanzhang>
2016-08-11 19:14:57 +0800

    simple auto commit

--- app/js/main_controllers.js
+++ app/js/main_controllers.js
@@ -185,6 +185,10 @@ function getHttpPromise($http, $rootScope, method, url, data, callback) {
 	if (!data["open_id"]) {
 		data["open_id"] = openId;
 	}
+	data["wechat_type"] = 1;//老公众账号
+	if(method === "GET"){
+		url = url + "&wechat_type=1"; 
+	}
 	return $http({
 		method: method,
 		headers: {
